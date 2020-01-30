24f921b460e95cabdca37ee868f268848a645858
Corey Hedberg <Cheddrs@users.noreply.github.com>
2020-01-29 19:11:03 -0500

    Added some JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
 /*
 This project was built by Corey Hedberg for the Software Development Mastermind program
 */
+
+let now = new Date();
+
+function day() {
+  let dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday"
+  ];
+  let theDay = now.getDay();
+  let nameOfDay = dayNames[theDay];
+
+  console.log(nameOfDay);
+}
+
+// Only need for the console
+// Remove when finished
+console.log("From the day function");
+day();
+
+function date() {
+  let monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  let theMonth = now.getMonth();
+  let nameOfMonth = monthNames[theMonth];
+  let theDate = now.getDate();
+  let theYear = now.getFullYear();
+
+  console.log(nameOfMonth);
+  console.log(theDate);
+  console.log(theYear);
+}
+
+// Only need for the console
+// Remove when finished
+console.log("From the date function");
+date();
